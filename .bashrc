5f04eb33e95fc20097cf9689e2b8d3bbf855659a
Asya Bergal <asyabergal@gmail.com>
2016-08-06 11:52:18 -0400

    Initial configs

---
+++ .bashrc
@@ -0,0 +1,14 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='ls --color=auto'
+PS1='[\u@\h \W]\$ '
+export PATH=$PATH:~/.cabal/bin:~/.xmonad/bin
+
+eval "$(rbenv init -)"
+
+PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,10 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 28"
+    xmonad =<< xmobar defaultConfig
