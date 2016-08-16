097f7f88d05e2d4275f59771a9ff9192635ebbf1
Asya Bergal <asyabergal@gmail.com>
2016-08-15 21:17:22 -0400

    Smartborders in xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
@@ -8,3 +9,5 @@ import System.IO
 main = do
     spawn "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 10 --transparent true --tint 0x000000 --height 28"
     xmonad =<< xmobar defaultConfig
+      { layoutHook = smartBorders . avoidStruts $ layoutHook defaultConfig
+      }
